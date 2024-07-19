044da690d33694ccb1af3c5960783564c8a42d94
mareeswari <jmareeswari0@gmail.com>
2024-07-19 16:23:10 +0530

    navbar setup

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <Fragment>
+      <div className="container-fluid banner-img">
+        <img src="images/Home.jpg" alt="image not found" className='img-fluid'/>
+      </div>
+
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 search-call">
+                <h3>Call Now</h3>
+                <i class="fa-solid fa-phone"></i>Call for detail information
+            </div>
+            <div className="col-lg-8 search">
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
   )
 }
