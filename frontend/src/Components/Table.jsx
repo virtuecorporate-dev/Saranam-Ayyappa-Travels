6475f9f78e24793fb85505776f6be1c1b95eb200
mareeswari <jmareeswari0@gmail.com>
2024-07-25 16:02:48 +0530

    admin page design

--- frontend/src/Components/Table.jsx
+++ frontend/src/Components/Table.jsx
@@ -8,16 +8,17 @@ function Table() {
     const dispatch = useDispatch();
     const cars = useSelector(state => state.Cars.Cars || [])
 
-    const handleDelete= (id)=>{
+    const handleDelete = (id) => {
         try {
             const response1 = axios.delete(`http://localhost:8000/api/v1/delete/${id}`)
-            dispatch (deleteCar(response1({id})))
+            dispatch(deleteCar(response1({ id })))
         } catch (error) {
-            
+
         }
     }
 
     useEffect(() => {
+
         const fetchData = async () => {
             try {
                 const response = await axios.get(`http://localhost:8000/api/v1/allCars`);
@@ -33,50 +34,68 @@ function Table() {
 
     return (
         <Fragment>
-            <Link to='/admin/create'>            <button>Add</button>
-            </Link>
-            <table class='table table-striped'>
-                <thead>
-                    <tr>
-                        <td>S.No</td>
-                        <td>carModel</td>
-                        <td>brand</td>
-                        <td>price</td>
-                        <td>seats</td>
-                        <td>availability</td>
-                        <td>imageUrl</td>
-                        <td>description</td>
-                        <td>category</td>
-                    </tr>
+               <div className="container mb-3">
+                <div className="table-add mt-4">
 
-                </thead>
-                <tbody>
-                    {cars.map((car, index) => {
-                        return <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{car.carModel}</td>
-                            <td>{car.brand}</td>
-                            <td>{car.price}</td>
-                            <td>{car.seats}</td>
-                            <td>{car.availability}</td>
-                            <img
-                                src={car.imageUrl}
-                                alt=""
-                                className="img-fluid"
-                                style={{ height: '200px', width: '200px' }}
-                            />
-                            <td>{car.description}</td>
-                            <td>
-                            {car.category}
-                                </td>   
-                                <Link to={`/edit/${car.id}`}><button>Edit</button>     
-                                </Link>
-                            <button onClick={()=>handleDelete(car.id)}>Delete</button>         
-                            </tr>
-                    })}
+                    <Link to='/admin/create'>
 
-                </tbody>
-            </table>
+                        <button><i class="fa-solid fa-plus"></i> Add</button>
+                    </Link>
+                    </div>
+                {
+                    cars.map((car, index) => (
+                        <div className="row car-detail mt-4" key={index}>
+                            <div className="col-lg-2 mt-2">
+                                <img
+                                    src={car.imageUrl}
+                                    alt=""
+                                    className="img-fluid"
+                                />                          
+                             </div>
+                            <div className="col-lg-9">
+                                <div className="row car-details">
+                                <div className="index col-lg-3">
+                                    <label htmlFor={`index-${index}`}>S.no: {index + 1}</label>
+                                </div>
+                                <div className="model col-lg-3">
+                                    <label htmlFor="brand">Model:{car.carModel}</label>
+                                </div>
+                                <div className="brand col-lg-3">
+                                    <label htmlFor="brand">Brand:{car.brand}</label>
+                                </div>
+                                <div className="price col-lg-3">
+                                    <label htmlFor="price">Price:{car.price}</label>
+                                </div>
+                                <div className="seats col-lg-3">
+                                    <label htmlFor="seats">Seats:{car.seats}</label>
+                                </div>
+                                <div className="availability col-lg-3">
+                                    <label htmlFor="availability">Availability:{car.availability}</label>
+                                </div>
+                                <div className="category col-lg-3">
+                                    <label htmlFor="category">Category:{car.category}</label>
+                                </div>
+                                <div className="description">
+                                    <label htmlFor="description">Description:{car.description}</label>
+                                </div>
+                               
+                                
+                                </div>
+                                
+                               
+                            </div>
+                            <div className="edit col-lg-1 mt-3">
+                                    <Link to={`/edit/${car.id}`}><button className='update'><i class="fa-solid fa-pen-to-square "></i>Edit</button>
+
+                                    </Link>
+                                    <button className='delete' onClick={() => handleDelete(car.id)}><i class="fa-solid fa-bucket "></i>Delete</button>
+                                </div>
+                        </div>
+                    ))
+                }
+            </div>
+
+
         </Fragment>
     )
 }
