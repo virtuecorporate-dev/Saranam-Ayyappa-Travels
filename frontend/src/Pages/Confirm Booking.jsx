e112d982cd80f718b605829c86663fb19a80936e
Sathiya kala <nsathiyakala6@gmail.com>
2024-07-26 23:01:38 +0530

    cab-confirm page

--- frontend/src/Pages/Confirm Booking.jsx
+++ frontend/src/Pages/Confirm Booking.jsx
@@ -33,30 +33,30 @@ const ConfirmBooking = () => {
         }));
     }
 
-    const BookNow = (e) => { 
+    const BookNow = (e) => {
         e.preventDefault();
         const bookingDetails = {
             ...state,
             ...form,
             selectedCab: state.selectedCab
-    }
+        }
 
-    fetch('http://localhost:8000/api/v1/sendemail', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ bookingDetails })
-      })
-        .then(response => response.json())
-        .then(data => {
-            alert("mail send")
-          console.log('Success:', data);
-          // You can add additional logic here, like showing a success message or redirecting the user.
+        fetch('http://localhost:8000/api/v1/sendemail', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ bookingDetails })
         })
-        .catch((error) => {
-          console.error( error);
-        });
+            .then(response => response.json())
+            .then(data => {
+                alert("mail send")
+                console.log('Success:', data);
+                // You can add additional logic here, like showing a success message or redirecting the user.
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
 
@@ -64,14 +64,20 @@ const ConfirmBooking = () => {
     return (
 
         <section className='confirmBooking-sec container'>
-            <h2>Confirm Your Cab</h2>
-            <div className='row'>
+            <h5 className='mt-4'>Confirm Your Cab</h5>
+            <div className='row mt-4'>
 
                 <div className='col-12'>
                     <div className='form-details' style={{ backgroundColor: "#a4a4e7", color: "white" }}>
 
                         <div className='d-flex justify-content-around location' >
-                            <h4>{state.from} => {state.to}</h4>
+                            <h4>
+                                {state.from} &nbsp; &nbsp;
+                                <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className ="bi bi-arrow-left-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5m14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5" />
+                                </svg> &nbsp; &nbsp;
+                                {state.to}
+                            </h4>
                             <h4>Date-{state.date} </h4> <h4>Time-{state.pickUpTime}</h4>
                         </div>
 
@@ -80,40 +86,44 @@ const ConfirmBooking = () => {
                 </div>
             </div>
 
-            <div className='row'>
+            <div className='row mt-5 mb-5'>
                 <div className='col-12'>
-                    <div className='container' style={{border:"1px solid red"}}>
+                    <div className='container p-0' >
                         <div className='row'>
-                            <div className='col-8'>
-                                <div className='selected-cab'>
-                                    <h4>{state.selectedCab.brand}</h4>
-                                    <p>{state.selectedCab.carModel}</p>
-                                    <img src={state.selectedCab.imageUrl} alt={state.selectedCab.brand} style={{ width: "250px" }} />
-                                </div>
-                                <div className='address-details'>
-                                    <p>Pick Up Location- {state.pickUpLocation}</p>
-                                    <p>Pick Up Time- {state.pickUpTime}</p>
-                                    {state.triptype === "Hourly Trip" &&
-                                        <p>Visiting Place- {state.visitingPlaces.join(', ')}</p>
-                                    }
-                                    {(state.triptype === "Round Trip" || state.triptype === "Hourly Trip") &&
-                                        <div>
-                                            <p>Drop Location- {state.dropLocation}</p>
-                                            <p>Drop Time- {state.dropTime}</p>
-                                        </div>
+                            <div className='col-12 col-lg-8'>
+                                <div className='selected-cab p-0' >
+                                    <div className='selected-cab-img'>
+                                        <img src={state.selectedCab.imageUrl} alt={state.selectedCab.brand} style={{ width: "250px" }} />
+                                    </div>
+                                    <div className='address-details'>
+                                        <h4 style={{fontWeight:"700"}}>{state.selectedCab.brand} {state.selectedCab.carModel}</h4>
+                                        <p></p>
+                                        <p> <b>Pick Up Location</b> - {state.pickUpLocation}</p>
+                                        <p> <b>Pick Up Time</b> - {state.pickUpTime}</p>
+                                        {state.triptype === "Hourly Trip" &&
+                                            <p> <b>Visiting Place</b> - {state.visitingPlaces.join(', ')}</p>
+                                        }
+                                        {(state.triptype === "Round Trip" || state.triptype === "Hourly Trip") &&
+                                            <div>
+                                                <p> <b>Drop Location</b> - {state.dropLocation}</p>
+                                                <p> <b>Drop Time</b> - {state.dropTime}</p>
+                                            </div>
 
-                                    }
+                                        }
+                                    </div>
+
                                 </div>
+
                             </div>
-                            <div className='col-4'>
+                            <div className='col-12 col-lg-4'>
                                 <div className='price-details'>
-                                    <h4>Total Price - ₹{state.selectedCab.price}</h4>
+                                    <h4>  <b> Total Price - ₹{state.selectedCab.price}</b> </h4>
                                 </div>
                                 <div className='traveller-details mt-4'>
                                     <h6>Fill Your Details </h6>
-                                    <form action="post" onSubmit={BookNow}>
+                                    <form action="post" onSubmit={BookNow} >
                                         <div className='field'>
-                                            <label htmlFor="name">Name</label>
+                                            <label className='mt-0' htmlFor="name">Name</label> <br />
                                             <input type="text"
                                                 id='name'
                                                 value={form.name}
@@ -121,7 +131,7 @@ const ConfirmBooking = () => {
                                                 required />
                                         </div>
                                         <div className='field'>
-                                            <label htmlFor="mobile">Mobile</label>
+                                            <label htmlFor="mobile">Mobile</label> <br />
                                             <input type="text"
                                                 id='mobile'
                                                 value={form.mobile}
@@ -129,7 +139,7 @@ const ConfirmBooking = () => {
                                                 required />
                                         </div>
                                         <div className='field'>
-                                            <label htmlFor="mail">Mail Id</label>
+                                            <label htmlFor="mail">Mail Id</label> <br />
                                             <input type="text"
                                                 id='mail'
                                                 value={form.mail}
