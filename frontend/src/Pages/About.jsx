51387cd5e0dff2a280846086a193f8cd647528f9
mareeswari <jmareeswari0@gmail.com>
2024-08-03 11:32:16 +0530

    error resolved

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -5,20 +5,59 @@ import { Link } from 'react-router-dom'
 export default function About() {
   return (
     <Fragment>
-      <div className="container-fluid p-0">
+      <div className="container-fluid p-0 about-banner">
         <div className="about-img ">
           <img src="./images/w4.jpg" alt="" />
         </div>
-        <div className="about-text">
+        <div className="about-text container-fluid mx-auto" >
+          <div className='row'>
+            <div className='col-10 col-md-6 about-content'>
+              <h2 style={{fontWeight:"600"}}>About Us</h2>
+              <h5 className='mt-3'>Be comfortable while you travel with our reliable bus rental.
+              </h5>
+                
+              <p className=' bredcrumb mt-3'>
+                    <Link to="/" style={{ color: "#deded7", textDecoration: "none" }}>Home/ </Link>
+                    <Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
+                </p>
+            </div>
+            <div className='col-2 col-md-6'></div>
+          </div>
 
-          <h2 className='text-center ' >
-            <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>Home/ </Link>
-            <Link to="/about" style={{ color: "#fff", textDecoration: "none" }}>About Us</Link>
-          </h2>   <h3>Be comfortable while you travel with our reliable Cab rental.</h3>
         </div>
 
       </div>
-      <main>
+      <section>
+        <div className="container-fluid mt-5 whoweare" style={{ backgroundColor: "transparent" }}>
+          <div className="row">
+            <div className='col-12 col-md-10 mx-auto pt-5 pb-5'>
+              <div className='container'>
+                <div className='row' style={{ color: "black" }}>
+                  <div className="col-lg-4">
+                    <h6>WHO WE ARE
+                    </h6>
+                    <h2 style={{ color: "#005395" }}>Enjoy the journey – travel with us</h2>
+                  </div>
+                  <div className="col-lg-8 mt-3 mt-lg-0">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam odio reprehenderit quos minima natus. Non est explicabo sed culpa iusto accusantium ducimus asperiores dolore voluptatibus, perferendis architecto aut, necessitatibus esse?
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni non eveniet, consequatur quas vel quaerat voluptate animi! Sequi incidunt ipsa odit hic, itaque reprehenderit exercitationem ipsum asperiores quam voluptatibus earum.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+          <div className="row gap-0" >
+            <div className='whoweare-img p-0'>
+              <img className='img-fluid' src="./images/who we are img.png" alt="whoweare" />
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      {/* <main>
         <div className="container-fluid  mt-5 about">
           <div className="row p-5 ">
             <div className='col-12 col-md-10 mx-auto pt-5 pb-5'>
@@ -30,7 +69,8 @@ export default function About() {
                     <h3>Enjoy the journey – travel with us</h3>
                   </div>
                   <div className="col-lg-8 about-row">
-                    <p>At our core, we are passionate about making every journey an unforgettable experience. We believe in the magic of travel, the thrill of exploring new destinations, and the joy of discovering the unknown. Our team is dedicated to providing exceptional service, ensuring that every step of your journey is seamless and enjoyable. From the moment you book with us to the time you return home, we are committed to creating memories that will last a lifetime. Whether you're embarking on a solo adventure, a family vacation, or a romantic getaway, travel with us and let us make your journey as delightful as the destination itself.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam odio reprehenderit quos minima natus. Non est explicabo sed culpa iusto accusantium ducimus asperiores dolore voluptatibus, perferendis architecto aut, necessitatibus esse?
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni non eveniet, consequatur quas vel quaerat voluptate animi! Sequi incidunt ipsa odit hic, itaque reprehenderit exercitationem ipsum asperiores quam voluptatibus earum.</p>
                   </div>
                 </div>
               </div>
@@ -48,13 +88,13 @@ export default function About() {
               <img src="./images/car3.png" class="img-fluid" alt="" />
             </div>
             <div className="col-lg-3 about-img">
-              <img src="./images/car4.png" class="img-fluid" alt="image not found" />
+              <img src="./images/car4.png" class="img-fluid" alt="" />
             </div>
           </div>
         </div>
-      </main>
-      <section className='container-fluid landing ' >
-        <div className='row' style={{ "margin-top": "200px" }}>
+      </main> */}
+      {/* <section className='container-fluid mt-5' >
+        <div className='row' >
           <div className='col-12 p-0'>
             <div className='banner'>
               <div className="banner-img">
@@ -65,33 +105,31 @@ export default function About() {
                   <div className="col-lg-8 col-sm-8 col-md-6 ">
                     <div className='about-banner-content p-5'>
                       <h1>Why Choose Us                                            </h1>
-                      <p>Choose us for cab rental for our well-maintained vehicles, professional drivers, transparent pricing, and exceptional customer service, ensuring a smooth and reliable experience every time.</p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                       <div className="row mt-4 about-banner-para">
                         <div className="col-lg-6 mt-4 ">
                           <h3 className='text-center'>
                             <i class="fa-solid fa-check"></i> &nbsp;
                             Safety & Security</h3>
-                            <p>
-                            We prioritize your safety with stringent vehicle checks and 24/7 support, ensuring a secure journey.
-                      </p>
+                          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quaerat iusto sequi saepe rerum quis et nesciunt quia expedita! Quas dolores cum aliquam iure tenetur, aut rerum? Officia, accusantium in.</p>
                         </div>
                         <div className="col-lg-6 mt-4">
                           <h3 className='text-center'>
                             <i class="fa-solid fa-check"></i> &nbsp;
                             On Time & Punctual</h3>
-                            <p>We pride ourselves on being on time and punctual, ensuring you reach your destination promptly and reliably every time.</p>
+                          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quaerat iusto sequi saepe rerum quis et nesciunt quia expedita! Quas dolores cum aliquam iure tenetur, aut rerum? Officia, accusantium in.</p>
                         </div>
                         <div className="col-lg-6 mt-4">
                           <h3 className='text-center'>
                             <i class="fa-solid fa-check"></i> &nbsp;
                             Professional Drivers</h3>
-                            <p>Our professional drivers are experienced, courteous, and dedicated to providing a safe and comfortable journey for you.</p>
+                          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quaerat iusto sequi saepe rerum quis et nesciunt quia expedita! Quas dolores cum aliquam iure tenetur, aut rerum? Officia, accusantium in.</p>
                         </div>
                         <div className="col-lg-6 mt-4">
                           <h3 className='text-center'>
                             <i class="fa-solid fa-check"></i> &nbsp;
                             Well Maintenance</h3>
-                            <p>We meticulously maintain our vehicles to ensure a safe, smooth, and reliable ride every time.</p>
+                          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates quaerat iusto sequi saepe rerum quis et nesciunt quia expedita! Quas dolores cum aliquam iure tenetur, aut rerum? Officia, accusantium in.</p>
                         </div>
                       </div>
                     </div>
@@ -109,7 +147,7 @@ export default function About() {
         </div>
 
 
-      </section>
+      </section> */}
       <section>
         <div className="container mb-3 about-ser">
           <div className="row about-service">
@@ -117,10 +155,9 @@ export default function About() {
               <img src="./images/carbanner1.png" alt="" className='img-fluid' />
             </div>
             <div className="col-lg-6">
-              <h3>We make sure every Cab is in excellent shape
-              </h3>
-              <p>We ensure every cab is in excellent shape through regular maintenance and thorough inspections. Our commitment to vehicle upkeep means you can trust in a safe, comfortable, and reliable ride every time you travel with us.
-              </p>
+              <h2>We make sure every bus is in excellent shape
+              </h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
               <div >
                 <ul className="about-point p-4">
                   <li><i class="fa-solid fa-check"></i>&nbsp;  Confirming a proper amount of engine oil and coolant</li>
@@ -137,11 +174,9 @@ export default function About() {
       <section>
         <div className="container-fluid">
           <div className="container p-5">
-            <h5 className='text-center' style={{ fontWeight: "700", "font-size": "30px" }}>OUR SERVICES </h5>
-            <h3 className='text-center  mt-4' style={{ fontWeight: "700", "font-size": "45px", "color": "#005395" }}> We Provide Best Services For You</h3>
-            <p className='text-center mt-4' style={{ fontWeight: "400", "font-size": "18px" }}>
-              Our cab rental service is dedicated to providing you with the best services tailored to your needs. Whether you need a quick ride across town, a comfortable long-distance journey, or transportation for a special event, we have you covered. With our well-maintained fleet and professional drivers, we ensure a smooth, safe, and reliable experience. Choose us for punctual, dependable service designed to meet your every transportation need.
-            </p>
+            <h6 className='text-center' style={{ fontWeight: "700" }}>OUR SERVICES </h6>
+            <h3 className='text-center  mt-4' style={{ fontWeight: "700", "color": "#005395" }}> We Provide Best Services For You</h3>
+            <p className='text-center mt-4' style={{ fontWeight: "400"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis inventore non dolorem delectus nesciunt harum perferendis, in, ullam assumenda distinctio libero, dolores molestias magnam rem. Ad doloribus labore dignissimos adipisci.</p>
 
           </div>
           <div className="row p-5 home-banner1-icons">
@@ -160,7 +195,7 @@ export default function About() {
 
                 <div className="card-body">
                   <h5 className="card-title text-center">One Way Trip</h5>
-                  <p className="card-text text-center"> For a seamless travel experience, choose our one-way trip service. Enjoy convenient and reliable transportation with professional drivers and well-maintained vehicles. Reach your destination comfortably and on time with ease.</p>
+                  <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
               </div>
             </div>
@@ -178,7 +213,7 @@ export default function About() {
                 </div>
                 <div className="card-body">
                   <h5 className="card-title text-center">Round Trip</h5>
-                  <p className="card-text text-center">Experience the convenience of our round trip service. Enjoy a comfortable and reliable journey with both outbound and return transportation included. Our professional drivers and well-maintained vehicles ensure a smooth trip from start to finish.</p>
+                  <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
               </div>
             </div>
@@ -196,7 +231,7 @@ export default function About() {
                 </div>
                 <div className="card-body">
                   <h5 className="card-title text-center">Hourly Trip</h5>
-                  <p className="card-text text-center">Opt for our hourly trip service for flexible and convenient transportation. Whether you need a vehicle for a few hours or an entire day, our professional drivers and well-maintained cars are at your service. Enjoy the freedom to travel at your own pace with reliable and comfortable rides.</p>
+                  <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
               </div>
             </div>
